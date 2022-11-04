d32f78576b5595cb77a3071a0fbced849a14a11a
Dany Cervantes <danycervantesq@gmail.com>
2022-11-04 02:20:14 -0400

    Update tribute.js
    Users can see the background image on title click.

--- tribute.js
+++ tribute.js
@@ -1,5 +1,36 @@
+const h1 = document.querySelector(".h1");
 let got = new Audio("assets/GOT.mp3");
 
+//allows viewing of background image on title click 
+h1.addEventListener("click", function() {
+
+    const body = document.querySelector(".body");
+    const contact = document.querySelector(".contact");
+    const header = document.querySelector(".header");
+    const main = document.querySelector(".main");
+    
+    body.style.height = "100vh";
+    body.style.overflow = "hidden";
+    contact.style.visibility = "hidden";
+    header.style.visibility = "hidden";
+    main.style.visibility = "hidden";
+    
+  //returns all to normal  
+  setTimeout(function() {
+
+    body.style.height = "auto";
+    body.style.overflow = "auto";
+    contact.style.visibility = "visible";
+    header.style.visibility = "visible";
+    main.style.visibility = "visible";
+  }, 10000); //waits 10 seconds to run
+
+});
+
+
+//keeping variable out of function prevents echo
 window.onclick = function() {
+
     got.play();
+
 };
